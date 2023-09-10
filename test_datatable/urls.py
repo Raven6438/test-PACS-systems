6a91c254c0f98ba71f42ffb04f36ed0d12da68d4
Artur <halilov.artur@gmail.com>
2023-09-10 19:50:18 +0500

    fix url path 'studies/'

--- test_datatable/urls.py
+++ test_datatable/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('init_db/', views.init_db, name='init_db'),
-    path('patients/', views.StudiesView.as_view(), name='get_studies'),
+    path('studies/', views.StudiesView.as_view(), name='get_studies'),
 ]
